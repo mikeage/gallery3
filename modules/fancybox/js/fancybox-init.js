4e70f0857c1c67c06f3d89adf9b9c4848dfe90ae
Mike Miller <github@mikeage.net>
2013-01-03 15:16:51 +0200

    Add fancybox from the codex

---
+++ modules/fancybox/js/fancybox-init.js
@@ -0,0 +1,24 @@
+// ******* Auto-generated file. Please do not change. ***
+
+$(document).ready(function() {
+  $("a.g-sb-preview").fancybox({
+     'overlayColor' : '#000'
+    ,'overlayOpacity' : 0.6
+    ,'titleShow' : false
+    ,'transitionIn' : 'elastic'
+    ,'transitionOut' : 'elastic'
+    ,'hideOnOverlayClick' : false
+    ,'cyclic' : true
+  });
+
+  $("a.g-fullsize-link").fancybox({
+     'overlayColor' : '#000'
+    ,'overlayOpacity' : 0.6
+    ,'titleShow' : false
+    ,'transitionIn' : 'elastic'
+    ,'transitionOut' : 'elastic'
+    ,'hideOnOverlayClick' : false
+    ,'cyclic' : true
+  });
+
+});
